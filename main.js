28f3abdcc2bd964d1d31947d43b6db4fd7628fbc
DevadathanB <74850270+DevadathanB@users.noreply.github.com>
2021-04-29 18:52:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(300, 300)
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300)
+}
